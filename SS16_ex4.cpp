82533a319e9f8065b7dd43cfcc21c156b988d3e3
Zeno2311 <shanks23k6@gmail.com>
2024-12-17 11:18:23 +0700

    Update SS16_ex4.cpp

--- SS16_ex4.cpp
+++ SS16_ex4.cpp
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <string.h>
-
 char arr[50];
 int main(){
-	void sigma();
-	sigma();
+	void sigma(char *arr);
+	sigma(arr);
 	return 0;
 }
 
-void sigma(){
-	char *x = arr;
+void sigma(char *arr){
 	printf("Moi ban nhap : ");
 	fgets(arr, 50, stdin);
-	for(int i = 0; i < strlen(arr) - 1; i++){
-		printf("%d\t", x[i]);
+	for(int i = 0; i < strlen(arr) - 1 ; i++){
+		printf("%c\t", arr[i]);
 	}
+	printf("\n");
 }
